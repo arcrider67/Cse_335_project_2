2ca91cd4ae26cff1a3c76799977b3c94a0a1f12d
arcrider67 <51412766+arcrider67@users.noreply.github.com>
2019-11-20 19:59:32 -0500

    Update GrowableImage.cpp

--- TreeLib/GrowableImage.cpp
+++ TreeLib/GrowableImage.cpp
@@ -11,5 +11,12 @@ CGrowableImage::CGrowableImage(const std::wstring &filename) {
 
 void CGrowableImage::Draw(Gdiplus::Graphics* graphics, double angle, double scale, double x, double y)
 {
-	//graphics->DrawImage(mImage, x, y);
+	auto state = graphics->Save();
+    graphics->TranslateTransform((float)x, (float)y);
+    graphics->RotateTransform((float)(angle * RtoD));
+    graphics->ScaleTransform((float)scale, (float)scale);
+    graphics->DrawImage(mImage.get(), -(int)mImage->GetWidth() / 2,
+        -(int)mImage->GetHeight(),
+        mImage->GetWidth(), mImage->GetHeight());
+    graphics->Restore(state);
 }
